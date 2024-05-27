79649bebe9b3a1e373a85ea45174c541ecc434d1
Jesus Elias Martinez <l20211768@tectijuana.edu.mx>
2024-05-27 04:10:35 +0000

    Actualización

--- movies/formR.py
+++ movies/formR.py
@@ -1,11 +1,13 @@
-#Utilizar formularios
 from django import forms
 
-#funciones existentes para pagina de reseña
-#Formulario para el reiew se debe llenar para enviar
 class ReviewF(forms.Form):
-    rating = forms.ChoiceField(label="⭐ Calificación",
-                                choices=[(i, str(i)) for i in range(1, 11)],  # Genera opciones del 1 al 10
-                                error_messages={"required": "La calificación es obligatoria"},
-                                widget=forms.Select(attrs={"class":"text-black"}))
-    review = forms.CharField(label="Reseña", widget=forms.Textarea(attrs={"class":"text-black", "rows": 3, "cols": 60}))
+    rating = forms.ChoiceField(
+        label="⭐ Score:",
+        choices=[(i, str(i)) for i in range(1, 11)],  # Genera opciones del 1 al 10
+        error_messages={"required": "La calificación es obligatoria"},
+        widget=forms.Select(attrs={"class": "text-black fixed-size w-full p-2 border border-gray-300 rounded-md"})
+    )
+    review = forms.CharField(
+        label="💭 Review:",
+        widget=forms.Textarea(attrs={"class": "text-black w-full p-2 border border-gray-300 rounded-md", "rows": 3})
+    )
